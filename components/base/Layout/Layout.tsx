940c290461827e3f72eb5098ae0ee36a1711f9ac
Harsh Gour <harshgour57@gmail.com>
2021-06-30 02:37:18 +0530

    fix: all changes height

--- components/base/Layout/Layout.tsx
+++ components/base/Layout/Layout.tsx
@@ -1,14 +1,16 @@
 import React, { FC, useState, useEffect } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import { LayoutProps as Props } from "../../../types";
 
 const Layout: FC<Props> = (props) => {
-  return (
-    <div className="w-full">
-      <Header />
-      <div className="container mx-auto">{props.children}</div>
-    </div>
-  );
+	return (
+		<div className='w-full h-full'>
+			<Header />
+			<div className='container mx-auto'>{props.children}</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default Layout;
