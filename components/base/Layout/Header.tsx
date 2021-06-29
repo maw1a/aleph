8ba2dbe02e85d8c4c3b0a4f9c60e39d66641aa81
Harsh Gour <harshgour57@gmail.com>
2021-06-30 02:36:39 +0530

    fix: Header add icons/Login

--- components/base/Layout/Header.tsx
+++ components/base/Layout/Header.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from "react";
 import SearchBar from "./SearchBar";
-import { Logo, Bookmarks, Notification } from "../../../icons";
+import { Logo, Bookmarks, Notification, User } from "../../../icons";
 import Button from "../Button";
 
 const Header: FC<any> = (props) => {
+	const [isLogged, setLogged] = React.useState(false);
 	return (
 		<nav className='shadow-md'>
 			<div className='container mx-auto px-2 py-3'>
@@ -13,12 +14,26 @@ const Header: FC<any> = (props) => {
 					</div>
 					<div className='flex'>
 						<SearchBar />
-						<Button icon={true}>
-							<Bookmarks className='h-5' fill='black' />
-						</Button>
-						<Button icon={true}>
-							<Notification className='h-5' fill='black' />
-						</Button>
+						{isLogged ? (
+							<div className='flex'>
+								<Button icon={true}>
+									<Bookmarks className='h-5' fill='black' />
+								</Button>
+								<Button icon={true}>
+									<Notification className='h-5' fill='black' />
+								</Button>
+								<Button icon={true} className='border-2'>
+									AM
+								</Button>
+							</div>
+						) : (
+							<div className='flex'>
+								<Button className='focus:outline-none'>Login</Button>
+								<Button className='bg-black text-white rounded-full px-5'>
+									Sign up
+								</Button>
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
