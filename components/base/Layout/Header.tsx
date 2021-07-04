3784c52f4e58c95c656ad297df08749a064dcecb
maw1a <ahmedmawia.dev@gmail.com>
2021-07-04 18:20:32 +0530

    fix: add Modal

--- components/base/Layout/Header.tsx
+++ components/base/Layout/Header.tsx
@@ -2,47 +2,56 @@ import React, { FC } from "react";
 import SearchBar from "./SearchBar";
 import { Logo, Bookmarks, Notification, User } from "../../../icons";
 import Button from "../Button";
+import ModalBase from "../Modal/ModalBase";
+import LoginModal from "../Modal/LoginModal";
 
 const Header: FC<any> = (props) => {
-	const [isLogged, setLogged] = React.useState(false);
-	return (
-		<nav className='shadow-md px-4'>
-			<div className='container mx-auto px-2 py-3'>
-				<div className='w-100 flex items-center justify-between'>
-					<div className='inline-flex'>
-						<Logo className='h-12' />
-					</div>
-					<div className='flex'>
-						<SearchBar />
-						{isLogged ? (
-							<div className='flex'>
-								<Button icon={true}>
-									<Bookmarks className='h-5' fill='black' />
-								</Button>
-								<Button icon={true}>
-									<Notification className='h-5' fill='black' />
-								</Button>
-								<Button icon={true} className='border-2'>
-									AM
-								</Button>
-							</div>
-						) : (
-							<div className='flex'>
-								<Button
-									className='focus:outline-none'
-									onClick={() => setLogged(true)}>
-									Login
-								</Button>
-								<Button className='bg-black text-white rounded-full px-5'>
-									Sign up
-								</Button>
-							</div>
-						)}
-					</div>
-				</div>
-			</div>
-		</nav>
-	);
+  const [isLogged, setLogged] = React.useState(false);
+  const [modalOpen, setModalOpen] = React.useState(false);
+  return (
+    <React.Fragment>
+      <nav className="shadow-md px-4">
+        <div className="container mx-auto px-2 py-3">
+          <div className="w-100 flex items-center justify-between">
+            <div className="inline-flex">
+              <Logo className="h-12" />
+            </div>
+            <div className="flex">
+              <SearchBar />
+              {isLogged ? (
+                <div className="flex">
+                  <Button icon={true}>
+                    <Bookmarks className="h-5" fill="black" />
+                  </Button>
+                  <Button icon={true}>
+                    <Notification className="h-5" fill="black" />
+                  </Button>
+                  <Button icon={true} className="border-2">
+                    AM
+                  </Button>
+                </div>
+              ) : (
+                <div className="flex">
+                  <Button
+                    className="focus:outline-none"
+                    onClick={() => setModalOpen(true)}
+                  >
+                    Login
+                  </Button>
+                  <Button className="bg-black text-white rounded-full px-5">
+                    Sign up
+                  </Button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+      <ModalBase onClose={() => setModalOpen(false)} open={modalOpen}>
+        <LoginModal />
+      </ModalBase>
+    </React.Fragment>
+  );
 };
 
 export default Header;
