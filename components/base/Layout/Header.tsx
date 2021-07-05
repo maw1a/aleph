ead9bdf366495c7f3e61d7357e156b49e51c70ce
maw1a <ahmedmawia.dev@gmail.com>
2021-07-06 01:13:47 +0530

    feat: Authflow add

--- components/base/Layout/Header.tsx
+++ components/base/Layout/Header.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from "react";
+import { useSession, signOut } from "next-auth/client";
 import SearchBar from "./SearchBar";
-import { Logo, Bookmarks, Notification, User } from "../../../icons";
+import { Logo, Bookmarks, Notification } from "../../../icons";
 import Button from "../Button";
 import ModalBase from "../Modal/ModalBase";
 import LoginModal from "../Modal/LoginModal";
+import Dropdown from "../Dropdown";
 
 const Header: FC<any> = (props) => {
-  const [isLogged, setLogged] = React.useState(false);
   const [modalOpen, setModalOpen] = React.useState(false);
+  const [profileDropdown, setProfileDropdown] = React.useState(false);
+  const [session, loading] = useSession();
   return (
     <React.Fragment>
       <nav className="shadow-md px-4">
@@ -18,30 +21,50 @@ const Header: FC<any> = (props) => {
             </div>
             <div className="flex">
               <SearchBar />
-              {isLogged ? (
-                <div className="flex">
-                  <Button icon={true}>
-                    <Bookmarks className="h-5" fill="black" />
-                  </Button>
-                  <Button icon={true}>
-                    <Notification className="h-5" fill="black" />
-                  </Button>
-                  <Button icon={true} className="border-2">
-                    AM
-                  </Button>
-                </div>
-              ) : (
-                <div className="flex">
-                  <Button
-                    className="focus:outline-none"
-                    onClick={() => setModalOpen(true)}
-                  >
-                    Login
-                  </Button>
-                  <Button className="bg-black text-white rounded-full px-5">
-                    Sign up
-                  </Button>
-                </div>
+              {!loading && (
+                <React.Fragment>
+                  {session ? (
+                    <div className="flex">
+                      <Button icon={true}>
+                        <Bookmarks className="h-5" fill="black" />
+                      </Button>
+                      <Button icon={true}>
+                        <Notification className="h-5" fill="black" />
+                      </Button>
+                      <Dropdown
+                        open={profileDropdown}
+                        anchor={
+                          <Button
+                            icon={true}
+                            className="border-2"
+                            onClick={() => setProfileDropdown(!profileDropdown)}
+                          >
+                            AM
+                          </Button>
+                        }
+                      >
+                        <Button
+                          className="text-gray-700 block px-4 py-2 text-sm"
+                          onClick={() => signOut()}
+                        >
+                          Sign out
+                        </Button>
+                      </Dropdown>
+                    </div>
+                  ) : (
+                    <div className="flex">
+                      <Button
+                        className="focus:outline-none"
+                        onClick={() => setModalOpen(true)}
+                      >
+                        Login
+                      </Button>
+                      <Button className="bg-black text-white rounded-full px-5">
+                        Sign up
+                      </Button>
+                    </div>
+                  )}
+                </React.Fragment>
               )}
             </div>
           </div>
