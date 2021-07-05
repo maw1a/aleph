d9c53d64f73a733f1063ead20dc89b1f293610b7
maw1a <ahmedmawia.dev@gmail.com>
2021-07-06 01:13:18 +0530

    fix: DropdownProps add

--- types.ts
+++ types.ts
@@ -1,32 +1,47 @@
 import React from "react";
 
 export interface LayoutProps {
-	pageProps?: any;
+  pageProps?: any;
 }
 
 export interface ButtonProps {
-	className?: string;
-	icon?: true | false;
-	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+  icon?: true | false;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export interface IconProps {
-	className?: string;
-	fill?: string;
+  className?: string;
+  fill?: string;
 }
 
 export interface TextSubtextProps {
-	className?: string;
-	text?: string;
-	textClassName?: string;
-	subtext?: string;
-	subtextClassName?: string;
+  className?: string;
+  text?: string;
+  textClassName?: string;
+  subtext?: string;
+  subtextClassName?: string;
 }
 
 export interface ModalBaseProps {
-	onClose: Function;
-	open: boolean;
-	onSuccess?: Function;
-	modalClassName?: string;
-	title?: string;
+  onClose: Function;
+  open: boolean;
+  onSuccess?: Function;
+  modalClassName?: string;
+  title?: string;
+}
+
+export interface DropdownProps {
+  open: boolean;
+  position?:
+    | "top"
+    | "bottom"
+    | "left"
+    | "right"
+    | "top-left"
+    | "top-right"
+    | "bottom-left"
+    | "bottom-right";
+  className?: string;
+  anchor: React.ReactNode;
 }
