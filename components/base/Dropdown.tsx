310162cb53b3504ec3533fc806acfcaf7e8903d5
maw1a <ahmedmawia.dev@gmail.com>
2021-07-06 01:12:07 +0530

    feat: Dropdown component add

---
+++ components/base/Dropdown.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import { DropdownProps } from "../../types";
+
+const Dropdown: FC<DropdownProps> = ({
+  anchor,
+  children,
+  open,
+  className = "",
+  position = "bottom-right",
+}) => {
+  const [display, setDisplay] = React.useState("");
+  React.useEffect(() => {
+    if (open) {
+      setDisplay("");
+    } else {
+      setTimeout(() => setDisplay("hidden"), 500);
+    }
+  }, [open]);
+
+  return (
+    <div className="relative inline-block">
+      {anchor}
+      <div
+        className={
+          "origin-top-right right-0 mt-2 absolute w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none transition ease-in-out duration-500 " +
+          (open
+            ? "transform opacity-100 scale-100"
+            : "transform opacity-0 scale-50") +
+          " " +
+          display +
+          " " +
+          className
+        }
+      >
+        <div className="py-1" role="none">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
