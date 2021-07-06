4f2f21a437e4d626324bb6059b5ebe068f32309f
maw1a <ahmedmawia.dev@gmail.com>
2021-07-07 01:06:56 +0530

    ref: clean className

--- components/base/Dropdown.tsx
+++ components/base/Dropdown.tsx
@@ -21,16 +21,14 @@ const Dropdown: FC<DropdownProps> = ({
     <div className="relative inline-block">
       {anchor}
       <div
-        className={
-          "origin-top-right right-0 mt-2 absolute w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none transition ease-in-out duration-500 " +
-          (open
+        className={[
+          "origin-top-right right-0 mt-2 absolute w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none transition ease-in-out duration-500",
+          open
             ? "transform opacity-100 scale-100"
-            : "transform opacity-0 scale-50") +
-          " " +
-          display +
-          " " +
-          className
-        }
+            : "transform opacity-0 scale-50",
+          display,
+          className,
+        ].join(" ")}
       >
         <div className="py-1" role="none">
           {children}
